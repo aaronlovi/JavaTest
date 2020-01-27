5bd9693039cbdc6b5cb111631fac3739407604db
Aaron Lovi <aaronlovi@hotmail.com>
2020-01-27 01:30:25 -0500

    Initial commit

---
+++ JavaTest/src/com/seven/javatest/Main.java
@@ -0,0 +1,19 @@
+package com.seven.javatest;
+
+public class Main {
+
+	private static String value = "one";
+
+	public static void main(String[] args) {
+		switch (value) {
+		case "one":
+			System.out.println("The value is one");
+			break;
+
+		default:
+			System.out.println("The value isn't one");
+			break;
+		}
+	}
+
+}
